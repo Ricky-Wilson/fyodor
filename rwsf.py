10ca596ecfa07458ed618c7e713710f02cd09e9b
Ricky Wilson <echoquote@gmail.com>
2017-07-22 02:03:29 -0500

    Added more doc string and fixed some PEP8 issues.

--- rwsf.py
+++ rwsf.py
@@ -1,7 +1,20 @@
+
+"""Find random web servers on the net.
+
+------------
+How it works
+------------
+
+* Generate a random non private IPv4 address.
+* Convert that address into a URL.
+* Use urllib2's getcode method to check if the url is live.
+"""
+
 import random
 import re
 import urllib2
 
+# This regular expression is used to eliminate private addresses.
 PRIVATE_ADDRESS = re.compile(r"^(" + '|'.join([
     r"127\.\d{1,3}\.\d{1,3}\.\d{1,3}", # PRIV_LOC
     r"10\.\d{1,3}\.\d{1,3}\.\d{1,3}", # PRIV_24
@@ -10,44 +23,76 @@ PRIVATE_ADDRESS = re.compile(r"^(" + '|'.join([
 ]) + r")$")
 
 
-def is_ip_private(ip):
+def is_ip_private(address):
     ''' Check id an IPv4 address is private.
-  
+
     This function uses a regular expression to Check
     if a IPv4 address is private.
-    
+
     - **parameters**, **types**, **return** and **return types**::
-      :param ip: an IPV4 address
-      :type ip: string
+      :param address: an IPV4 address
+      :type address: string
       :return: return None if the address is private and _sre.SRE_Match if it is
       :rtype: None or _sre.SRE_Match
     '''
-    return PRIVATE_ADDRESS.match(ip)
+    return PRIVATE_ADDRESS.match(address)
 
 
-def generate_addresses(n):
+def generate_addresses(amount):
+    """ Generate a set list of random IPv4 addresses.
+
+    **parameters**
+    :param amount: The number of addresses to generate.
+    :type amount: int
+    :return: a set list of random IPv4 addresses.
+    :rtype: set
+    """
     addresses = set([])
-    while len(addresses) is not n:
-      address = '.'.join(str(random.randrange(256)) for _ in range(4))
-      if not is_ip_private(address):
-        addresses.add(address)
+    while len(addresses) is not amount:
+        address = '.'.join(str(random.randrange(256)) for _ in range(4))
+        if not is_ip_private(address):
+            addresses.add(address)
     return addresses
 
 
-def generate_urls(n, scheme='http', path='/'):
-    return ("{}://{}{}".format(scheme, address, path) for address in generate_addresses(n))
+def generate_urls(amount, scheme='http', path='/'):
+    """ Generate a list of random URL's.
+
+    **parameters**
+    :param amount: The number of URL's to generate.
+    :param scheme: the url scheme to use, the default scheme is http.
+    :param path: the url path to use, default is "/" aka root
+    :rtype: generator
+    """
+    return ("{}://{}{}".format(scheme, address, path) for address in generate_addresses(amount))
 
 
-def is_live(url, timeout=5):
+def is_live(url, timeout=3):
+    """Check if a URL is live.
+
+    This function uses urllib2's getcode method to check
+    if a URL is live.
+
+    **parameters**
+    :param url: The url to check.
+    :param timeout: How long to wait for a connection, the default is 3 seconds
+    :type url: string
+    :type timeout: int or float
+    :rtype: bool
+    """
     try:
-        return urllib2.urlopen(url, timeout=timeout).getcode()
-    except urllib2.URLError, e:
+        if urllib2.urlopen(url, timeout=timeout).getcode() is 200:
+            return
+        else:
+            return False
+    except urllib2.URLError:
         return False
-    
-    
-def scan(n, timeout=1, **kwargs):
-    for url in generate_urls(n, **kwargs):
-       print is_live(url, timeout=timeout)
 
-scan(100)
 
+def scan(amount, timeout=1, **kwargs):
+    for url in generate_urls(amount, **kwargs):
+        if is_live(url, timeout=timeout):
+            print url
+
+if __name__ == '__main__':
+    scan(100)
